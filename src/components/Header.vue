52dced3a540492d510bc317e3b55e570341c332c
BigOoga <edoardo.ruffi@gmail.com>
2021-09-06 19:18:49 +0200

    Start

---
+++ src/components/Header.vue
@@ -0,0 +1,82 @@
+!<template>
+  <header>
+    <div id="logo">
+      <img src="@/assets/img/dc-logo.png" alt="Logo" />
+    </div>
+
+    <nav>
+      <ul>
+        <li v-for="(link, index) in links" :key="index">
+          <a :href="link.url"> {{ link.text }}</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      links: [
+        {
+          text: "Characters",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Comics",
+          url: "#",
+          current: true,
+        },
+        {
+          text: "Movies",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "TV",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Games",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Collectibles",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Videos",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Fans",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "News",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Shop",
+          url: "#",
+          current: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+nav ul li {
+}
+</style>
