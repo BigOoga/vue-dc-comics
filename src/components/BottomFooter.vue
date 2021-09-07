5aec0468425298fca6f00995296749922d439390
BigOoga <edoardo.ruffi@gmail.com>
2021-09-07 15:25:39 +0200

    Checkpoint

--- src/components/BottomFooter.vue
+++ src/components/BottomFooter.vue
@@ -1,14 +1,25 @@
 <template>
   <div id="bottom-footer">
-    <div class="d-flex justify-content-around align-items-center">
+    <div class="d-flex justify-content-around align-items-center h-100">
       <div class="btn-signup text-uppercase">Sign up now!</div>
       <div class="links">
         <ul>
-          <li><a href="">Follow us</a></li>
-          <li><a href="">fa</a></li>
-          <li><a href="">fa</a></li>
-          <li><a href="">fa</a></li>
-          <li><a href="">fa</a></li>
+          <li><a class="fs-2" href="">Follow us</a></li>
+          <li>
+            <a href=""><img src="@/assets/img/footer-facebook.png" alt=""/></a>
+          </li>
+          <li>
+            <a href=""><img src="@/assets/img/footer-twitter.png" alt=""/></a>
+          </li>
+          <li>
+            <a href=""><img src="@/assets/img/footer-youtube.png" alt=""/></a>
+          </li>
+          <li>
+            <a href=""><img src="@/assets/img/footer-pinterest.png" alt=""/></a>
+          </li>
+          <li>
+            <a href=""><img src="@/assets/img/footer-periscope.png" alt=""/></a>
+          </li>
         </ul>
       </div>
     </div>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,51 +1,51 @@
 <template>
   <footer>
-    <div class="container d-flex">
-      <div class="container30 d-flex">
-        <div>
-          <h2>DC Comics</h2>
-          <ul>
-            <li><a href="">Characters</a></li>
-            <li><a href="">Comics</a></li>
-            <li><a href="">Movies</a></li>
-            <li><a href="">TV</a></li>
-            <li><a href="">Games</a></li>
-            <li><a href="">Videos</a></li>
-            <li><a href="">News</a></li>
+    <div class="container d-flex ">
+      <div class="container35 d-flex py-5">
+        <div class="px-2">
+          <h2 class="py-2">DC Comics</h2>
+          <ul class="fs-7">
+            <li class="py-1"><a href="">Characters</a></li>
+            <li class="py-1"><a href="">Comics</a></li>
+            <li class="py-1"><a href="">Movies</a></li>
+            <li class="py-1"><a href="">TV</a></li>
+            <li class="py-1"><a href="">Games</a></li>
+            <li class="py-1"><a href="">Videos</a></li>
+            <li class="py-1"><a href="">News</a></li>
           </ul>
-          <h2>Shop</h2>
-          <ul>
-            <li><a href="">Shop DC</a></li>
-            <li><a href="">Shop DC Collectibles</a></li>
-          </ul>
-        </div>
-        <div>
-          <h2>DC</h2>
-          <ul>
-            <li><a href="">Terms Of Use</a></li>
-            <li><a href="">Privacy Policy</a></li>
-            <li><a href="">Ad Choices</a></li>
-            <li><a href="">Advertising</a></li>
-            <li><a href="">Jobs</a></li>
-            <li><a href="">Subscriptions</a></li>
-            <li><a href="">CPSC Certificates</a></li>
-            <li><a href="">Ratings</a></li>
-            <li><a href="">Shop Help</a></li>
-            <li><a href="">Contact Us</a></li>
+          <h2 class="py-2">Shop</h2>
+          <ul class="fs-7">
+            <li class="py-1"><a href="">Shop DC</a></li>
+            <li class="py-1"><a href="">Shop DC Collectibles</a></li>
           </ul>
         </div>
-        <div>
-          <h2>Sites</h2>
-          <ul>
-            <li><a href="">DC</a></li>
-            <li><a href="">MAD Magazine</a></li>
-            <li><a href="">DC Kids</a></li>
-            <li><a href="">DC Universe</a></li>
-            <li><a href="">DC Power Visa</a></li>
+        <div class="px-2">
+          <h2 class="py-2">DC</h2>
+          <ul class="fs-7">
+            <li class="py-1"><a href="">Terms Of Use</a></li>
+            <li class="py-1"><a href="">Privacy Policy</a></li>
+            <li class="py-1"><a href="">Ad Choices</a></li>
+            <li class="py-1"><a href="">Advertising</a></li>
+            <li class="py-1"><a href="">Jobs</a></li>
+            <li class="py-1"><a href="">Subscriptions</a></li>
+            <li class="py-1"><a href="">CPSC Certificates</a></li>
+            <li class="py-1"><a href="">Ratings</a></li>
+            <li class="py-1"><a href="">Shop Help</a></li>
+            <li class="py-1"><a href="">Contact Us</a></li>
+          </ul>
+        </div>
+        <div class="px-2">
+          <h2 class="py-2">Sites</h2>
+          <ul class="fs-7">
+            <li class="py-1"><a href="">DC</a></li>
+            <li class="py-1"><a href="">MAD Magazine</a></li>
+            <li class="py-1"><a href="">DC Kids</a></li>
+            <li class="py-1"><a href="">DC Universe</a></li>
+            <li class="py-1"><a href="">DC Power Visa</a></li>
           </ul>
         </div>
       </div>
-      <div class="container70"></div>
+      <div class="container65 py-5"></div>
     </div>
   </footer>
 </template>
